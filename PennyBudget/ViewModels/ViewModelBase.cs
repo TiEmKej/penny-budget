9f7e145715e7050e6b824ac8c7f2c99361ca5435
Miłosz Piekarski <milosz.piekarski2003@gmail.com>
2026-06-04 21:30:30 +0200

    Init

---
+++ PennyBudget/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PennyBudget.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ PennyBudget/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PennyBudget.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

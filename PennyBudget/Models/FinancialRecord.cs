89e90bb6530c27e3edfb79c44b2f4fe12cc588d3
Miłosz Piekarski <milosz.piekarski2003@gmail.com>
2026-06-13 17:22:21 +0200

    IsCash field for record with db migration

--- PennyBudget/Models/FinancialRecord.cs
+++ PennyBudget/Models/FinancialRecord.cs
@@ -6,12 +6,13 @@ namespace PennyBudget.Models;
 public class FinancialRecord
 {
     public int Id { get; set; }
-    public int CategoryId { get; set; }
-    public RecordCategory Category { get; set; } = null!;
     public string Description { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
     public decimal Amount { get; set; }
-    [NotMapped] public decimal AmountInYourCurrency => Amount * CurrencyRate;
     public string Currency { get; set; } = "PLN";
     public decimal CurrencyRate { get; set; } = 1.0m;
-    public DateTime Date { get; set; } = DateTime.Now;
+    [NotMapped] public decimal AmountInYourCurrency => Amount * CurrencyRate;
+    public int CategoryId { get; set; }
+    public RecordCategory Category { get; set; } = null!;
+    public bool IsCash  { get; set; }
 }
